16a7778943d9a9b5f4d1510e944430ec31a39744
rasca0027 <rasca0027@gmail.com>
2016-03-24 22:50:29 +0800

    display location and change icons

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, StringProperty
 import requests
 
 APP_ID = '93b4bc2c1a8637730962f114155febb5'
@@ -17,11 +17,15 @@ def get_weather(cityname):
 
 class WeatherDashboard(BoxLayout):
     degree = NumericProperty(0)    
-    
+    location = StringProperty('Taipei')
+
     def __init__(self, **kwargs):
         super(WeatherDashboard, self).__init__(**kwargs)
         self.degree = get_weather('Taipei')
 
+    def change_location(self, cityname):
+        self.location = cityname
+        print cityname
 
 class MyApp(App):
 
