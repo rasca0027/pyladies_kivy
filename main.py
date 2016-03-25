12ee92b9ad1e615037ec3b057687ffcc078b7d93
rasca0027 <rasca0027@gmail.com>
2016-03-26 02:48:26 +0800

    step 1 sample code

--- main.py
+++ main.py
@@ -1,33 +1,10 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import NumericProperty, StringProperty
-import requests
-
-APP_ID = '93b4bc2c1a8637730962f114155febb5'
-
-def get_weather(cityname):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s'
-    r = requests.get(url % (cityname, APP_ID))
-    r = r.json()
-    temp = r['main']['temp']
-    # convert to celcius degree
-    degree = temp - 273
-    humidity = r['main']['humidity']
-    return int(degree), humidity
 
 
 class WeatherDashboard(BoxLayout):
-    degree = NumericProperty(0)
-    humid = NumericProperty(0)
-    location = StringProperty('Taipei')
+    pass
 
-    def __init__(self, **kwargs):
-        super(WeatherDashboard, self).__init__(**kwargs)
-        self.degree, self.humid = get_weather('Taipei')
-
-    def change_location(self, cityname):
-        self.location = cityname
-        print cityname
 
 class MyApp(App):
 
