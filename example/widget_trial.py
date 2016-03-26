f5d7d0e1e500e053cc0b1a2ce5998e3717f41b0d
tim <tim.yellow@gmail.com>
2016-03-26 17:54:03 +0800

    trial of toggle widget

--- example/widget_trial.py
+++ example/widget_trial.py
@@ -1,13 +1,16 @@
 # example/basic_widget.py
 from __future__ import print_function
 from kivy.app import App
-from kivy.uix.togglebutton import ToggleButton
+from kivy.uix.button import Button
 
 
 class MyApp(App):
 
     def build(self):
-        button = ToggleButton(text='PyLadies Rocks')
+        button = Button(text='PyLadies Rocks',
+                        font_size=40,
+                        color=(0, 0, 254, 1),
+                        background_color=(1, 0, 0, 1))
 
         button.on_press = lambda: print("yo")
         return button
