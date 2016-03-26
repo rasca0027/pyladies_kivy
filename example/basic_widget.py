6875dbb030b06464536ff1b0d29385fe3274336f
tim <tim.yellow@gmail.com>
2016-03-26 17:41:50 +0800

    update a basic_widget sample

--- example/basic_widget.py
+++ example/basic_widget.py
@@ -1,13 +1,18 @@
-import kivy
+from __future__ import print_function
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.button import Button
 
 
 class MyApp(App):
 
     def build(self):
-        label = Label(text='PyLadies Rocks')
-        return label
+        button = Button(text='PyLadies Rocks',
+                        font_size=40,
+                        color=(0, 0, 254, 1),
+                        background_color=(1, 0, 0, 1))
+
+        button.on_press = lambda: print("yo")
+        return button
 
 
 app = MyApp()
