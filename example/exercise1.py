1728eaae4fb4ef9b884d8c6813e94a99701a406d
rasca0027 <rasca0027@gmail.com>
2016-04-16 02:28:29 +0800

    add grid examples

---
+++ example/exercise1.py
@@ -0,0 +1,10 @@
+# example/exercise1.py
+from kivy.app import App
+
+
+class Exercise1App(App):
+    pass
+
+
+app = Exercise1App()
+app.run()
---
+++ example/grid_example.py
@@ -0,0 +1,10 @@
+# example/grid_example.py
+from kivy.app import App
+
+
+class Grid_ExampleApp(App):
+    pass
+
+
+app = Grid_ExampleApp()
+app.run()
